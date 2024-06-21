29638ab20b8df6aff3fc290c47e0cb23c8388d1e
Janjanny <janjanbeboso0912@gmail.com>
2024-06-21 21:50:50 +0800

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 
-import './App.css'
 
 function App() {
 
   return (
     <>
-      <p className='text-3xl font-bold underline'>Hello World</p>
+      
+      Categories
+      Bookmarks
+      New Button
+      Footer
     </>
   )
 }
