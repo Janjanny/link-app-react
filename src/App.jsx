d52a1f1bcc1ebe23c888d8e48a33b28c39cc2cc0
Janjanny <janjanbeboso0912@gmail.com>
2024-06-21 23:28:44 +0800

    add search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import Bookmarks from "./components/Bookmarks"
-import Categories from "./components/Categories"
-import Footer from "./components/Footer"
-import Navbar from "./components/Navbar"
+import Bookmarks from "./components/Bookmarks";
+import Categories from "./components/Categories";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
     <>
-      <Navbar/>
-      <main className=" w-full mx-auto">
-        <Categories/>
-        <Bookmarks/>
+      <Navbar />
+      <main className=" w-full min-h-screen mx-auto bg-black">
+        <Categories />
+        <Bookmarks />
         New Button
       </main>
-      <Footer/>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,36 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div className='w-full bg-black text-white'>
-      <nav className='flex w-11/12 mx-auto justify-between items-center py-3'>
-        <h1 className=' font-bold text-2xl'>Links</h1>
-        <div className='flex gap-3'>
-          <div className=' bg-red-400'>
-            <input type="text" className=' bg-black focus:outline-none outline-1 text-start' placeholder='search...' />
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-</svg>
-    
+    <div className="w-full bg-black text-white py-3 border-b border-gray border-solid">
+      <nav className="flex w-11/12 mx-auto justify-between items-center py-3">
+        <h1 className=" font-bold text-3xl">Links</h1>
+        <div className="flex gap-3">
+          <div className="flex border-solid border border-gray py-2 px-4 text-sm rounded-2xl">
+            <input
+              type="text"
+              className=" bg-black focus:outline-none outline-1 text-start w-[180px]"
+              placeholder="Search..."
+            />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-6 cursor-pointer"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+              />
+            </svg>
           </div>
         </div>
       </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
