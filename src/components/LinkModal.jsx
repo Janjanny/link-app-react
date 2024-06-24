7ae1e70a1b238a1f30f7f765f0452c0ddfb69419
Janjanny <janjanbeboso0912@gmail.com>
2024-06-25 00:46:52 +0800

    implementing function

--- src/components/LinkModal.jsx
+++ src/components/LinkModal.jsx
@@ -10,7 +10,6 @@ const LinkModal = ({isActive, setActive}) => {
   const [category, setCategory] = useState(categories[0]);
 
   const handleSubmit = async (event) => {
-    event.preventDefault();
     await addBookmark(url); // Call addBookmark with the URL and category
     setActive(!isActive); // Close the modal
   };
@@ -21,11 +20,11 @@ const LinkModal = ({isActive, setActive}) => {
       {/* blur bg */}
       <div className='absolute inset-0 bg-black bg-opacity-50 backdrop-blur-sm cursor-pointer' onClick={() => {setActive(!isActive)}}></div>
       <div className='relative z-10 px-[2.5rem] py-[3rem] bg-black-card text-white rounded-md shadow'>
-        <label for="price" class="block text-sm font-medium leading-6 text-gray-900">Link</label>
-        <input type="text" name="price" id="price" class="block rounded-md border-0 py-1.5 pl-3  ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 text-black w-[20rem]" placeholder="Enter link" value={url} onChange={(e) => {setUrl(e.target.value)}}></input>
+        <label className="block text-sm font-medium leading-6 text-gray-900">Link</label>
+        <input type="text" name="price" id="price" className="block rounded-md border-0 py-1.5 pl-3  ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 text-black w-[20rem]" placeholder="Enter link" value={url} onChange={(e) => {setUrl(e.target.value)}}></input>
 
-        <label for="country" class="block text-sm font-medium leading-6 text-gray-900 mt-8">Category</label>
-        <select id="country" name="country" autocomplete="country-name" class="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6 text-black pl-3" value={category} onChange={(e) => {setCategory(e.target.value)}}>
+        <label  className="block text-sm font-medium leading-6 text-gray-900 mt-8">Category</label>
+        <select  className="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6 text-black pl-3" value={category} onChange={(e) => {setCategory(e.target.value)}}>
               {categories.map((category, index) => (<option key={index}>{category}</option>))}
             </select>
 
