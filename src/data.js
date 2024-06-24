52a6483053d6496415f70a0434de829ac5a0c42d
Janjanny <janjanbeboso0912@gmail.com>
2024-06-24 21:42:05 +0800

    add functions

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-const bookmarks = [ 
+const initialBookmarks = [ 
     {
     name: 'Facebook', 
     link: 'https://www.facebook.com/', 
@@ -15,8 +15,51 @@ const bookmarks = [
     desc: 'From breaking news and entertainment to sports and politics, get the full story with all the live commentary.'},
 ]
 
-const categoryList = ["All", "Socials", "Personal", "Education", "Shopping", "Games", "Music"];
+const initialCategories = ["All", "Socials", "Personal", "Education", "Shopping", "Games", "Music"];
 
+// save to localStorage if not present
+if(!localStorage.getItem('bookmarks')) {
+    localStorage.setItem('bookmarks', JSON.stringify(initialBookmarks))
+}
+
+if(!localStorage.getItem('categories')) {
+    localStorage.setItem('categories', JSON.stringify(initialCategories))
+}
+
+// functions for getting data
+export function getBookmarks() {
+    return JSON.parse(localStorage.getItem('bookmarks') || [])
+}
+
+export function getCategories() {
+    return JSON.parse(localStorage.getItem('categories') || [])
+}
+
+// function for adding data 
+export function addBookmark(bookmark) {
+    const bookmarks = getBookmarks();
+    bookmarks.push(bookmark);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+}
+
+export function addCategory(category) {
+    const categories = getCategories();
+    categories.push(category);
+    localStorage.setItem('categories', JSON.stringify(categories));
+}
+
+//function for modifying the data 
+export function removeBookmark(index) {
+    const bookmarks = getBookmarks();
+    bookmarks.splice(index, 1);
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+}
+
+export function removeCategory(index) {
+    const categories = getCategories();
+    categories.splice(index, 1);
+    localStorage.setItem('categories', JSON.stringify(categories))
+}
 
 
 export {bookmarks, categoryList};
