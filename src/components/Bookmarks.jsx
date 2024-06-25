a34cd655eaa1d7c2affe8e61d2f4fc28836f581c
Janjanny <janjanbeboso0912@gmail.com>
2024-06-25 19:50:40 +0800

    try add use effect

--- src/components/Bookmarks.jsx
+++ src/components/Bookmarks.jsx
@@ -5,6 +5,7 @@ import { getBookmarks } from '../data'
 
 const Bookmarks = () => {
   const bookmarks = getBookmarks()
+
   return (
     <div className='w-11/12 mx-auto my-5 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 col-auto gap-4'>
       {bookmarks.map((bookmark, index) => (<Card name={bookmark.title} desc={bookmark.description} link={bookmark.url} img={bookmark.image} key={index}/>) )}
